ed63490e58307e6bd944a6832e6ce5bd7e81c3ec
Fedor Nikonchuk <69729440+fedosssss@users.noreply.github.com>
2022-10-13 15:50:52 +0300

    Workable qr code generator on Python

---
+++ qr_code_generator.py
@@ -0,0 +1,9 @@
+import qrcode
+
+website_link = 'https://www.python.org/downloads/'
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr.add_data(website_link)
+qr.make()
+
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+img.save('qrcode_picture.png')
